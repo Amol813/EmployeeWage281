1758682f3fd5e57f5b11664ad638b06ff16d4db0
unknown <amoljadhav>
2023-05-06 20:58:48 +0530

    [Amol]Welcome to Employee is Present or Absent

--- EmployeeWage281/Program.cs
+++ EmployeeWage281/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace EmployeeWage281
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 1;
             Console.WriteLine("Welcome to the EmployeeWage Computation Problem!");
+            Random random = new Random();
+            int randomInput = random.Next(0, 2);
+            if (IS_PRESENT == randomInput)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+
         }
     }
 }
