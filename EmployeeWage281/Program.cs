3ef89bb352f71bd4df34132094d45f66fd0474e2
unknown <amoljadhav>
2023-05-08 20:31:07 +0530

    [Amol]Employee Wage By Switch Case

--- EmployeeWage281/Program.cs
+++ EmployeeWage281/Program.cs
@@ -12,22 +12,18 @@
             Console.WriteLine("Welcome to the EmployeeWage Computation Problem!");
             Random random = new Random();
             int randomInput = random.Next(0, 3); //0 or 1 or 2
-            if (FULL_TIME == randomInput)
+           switch (randomInput)
             {
-                empHrs = 8;
-                Console.WriteLine("Employee is Present");
-            }
-            else 
-
-
-            if (PART_TIME == randomInput)
-            {
-                empHrs = 4;
-                Console.WriteLine("PartTime Employee is Present");
-            } 
-            else
-            {
-                Console.WriteLine("Employee is absent");
+                case FULL_TIME:
+                    empHrs = 8;
+                    Console.WriteLine("FullTime Employee is Present");
+                    break;
+                case PART_TIME:
+                    empHrs = 4;
+                    Console.WriteLine("PartTime Employee is Present");
+                    break;
+                default:
+                    break;
             }
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee Wage per Day:{0}", empWage);
