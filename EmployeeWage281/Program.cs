bec37d9bba7631a8e50e33d13ac71d3400f42948
unknown <amoljadhav>
2023-05-15 23:32:05 +0530

    [Amol]Wages of Total Working Hrs or days is Reached for Month

--- EmployeeWage281/Program.cs
+++ EmployeeWage281/Program.cs
@@ -8,34 +8,39 @@
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAY = 20;
+            const int MAX_WORKING_HRS = 100;
             int empHrs = 0;
             int empWage = 0;
             int totalwage = 0;
+            int day = 1;
+            int totalHrs = 0;
             Console.WriteLine("Welcome to the EmployeeWage Computation Problem!");
             Random random = new Random();
-            for (int day = 0;day< MAX_WORKING_DAY;day++)
+            while (day <= MAX_WORKING_DAY && totalHrs<=MAX_WORKING_HRS)
             {
                 int randomInput = random.Next(0, 3); //0 or 1 or 2
                 switch (randomInput)
                 {
                     case FULL_TIME:
                         empHrs = 8;
-                      //  Console.WriteLine("FullTime Employee is Present");
+                        //  Console.WriteLine("FullTime Employee is Present");
                         break;
                     case PART_TIME:
                         empHrs = 4;
-                      //  Console.WriteLine("PartTime Employee is Present");
+                        //  Console.WriteLine("PartTime Employee is Present");
                         break;
                     default:
                         break;
                 }
                 empWage = EMP_RATE_PER_HR * empHrs;
                 totalwage += empWage; // totalWage=totalWage+empWage;
-                Console.WriteLine("Employee Wage per Day{0}:{1}",day, empWage);
+                day++;
+                totalHrs += empHrs;
+                Console.WriteLine("Employee Wage per Day{0}:{1}", day, empWage);
             }
-            Console.WriteLine("Totalwage for {0} day is: day is {1}",MAX_WORKING_DAY, totalwage);
+            Console.WriteLine("Totalwage for {0} days and Hrs:{1} is:{2}", (-1),totalHrs,totalwage);
             Console.ReadLine();
         }
-         
     }
+     
 }
